7207eb39fae08b9cea3da20fe8148ed2456822ff
Cirdans-Home <f.durastante@na.iac.cnr.it>
2022-04-16 16:24:21 +0200

    Added download of IFISS 3.6

--- download.sh
+++ download.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-echo "Downloading bmd_mfem code"
-wget http://personal.cityu.edu.hk/~szhang26/bdm_mfem.zip
-unzip bdm_mfem.zip
+if [ ! -d "ifem" ]
+then
+ echo "Downloading bmd_mfem code"
+ wget http://personal.cityu.edu.hk/~szhang26/bdm_mfem.zip
+ unzip bdm_mfem.zip
+else
+ echo "BMD_MFEM is present"
+fi
+
+if  [ ! -d "ifiss3.6" ]
+then
+ echo "Downloading IFISS"
+ wget https://personalpages.manchester.ac.uk/staff/david.silvester/ifiss/ifiss3.6.tar.gz
+ tar -xzf ifiss3.6.tar.gz
+else
+ echo "IFISS is present"
+fi
